ef15f9270efcc98e9e3c88447260ad9523040385
Ganesh Pawar <pawarganeshb123@gmail.com>
2025-01-01 12:39:53 +0530

    Initial commit

---
+++ REST-API-Products/src/main/java/com/ganesh/rest/RestApiProductsApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiProductsApplication.class, args);
+	}
+
+}
